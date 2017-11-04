e59f0652b9be87b06a5c2d75aef61a915e7b7968
BumagniyPacket <bumagniypacket@yandex.ru>
2017-11-04 13:46:12 +0300

    Comment model.manager

--- blog/comments/models.py
+++ blog/comments/models.py
@@ -3,10 +3,17 @@ from django.db import models
 from blog.posts.models import Post
 
 
+class CommentManager(models.Manager):
+    def approved(self):
+        return super().filter(approved=True).order_by('-timestamp')
+
+
 class Comment(models.Model):
     class Meta:
         verbose_name = 'Блог - комментарий'
         verbose_name_plural = 'Блог - комментарии'
+
+    objects = CommentManager()
 
     post = models.ForeignKey(Post, related_name='comments')
     author = models.CharField(max_length=50, default='anon')
