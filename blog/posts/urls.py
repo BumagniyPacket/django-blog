69a85e4f842fcd103a9982f4ce12a96bf5a6a593
BumagniyPacket <bumagniypacket@yandex.ru>
2017-11-04 22:36:57 +0300

    CBV post detail

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 
-from .views import PostsList, add_comment, post_create, post_delete, post_detail, post_update
+from .views import PostDetail, PostsList, add_comment, post_create, post_delete, post_update
 
 urlpatterns = [
     url(r'^$', PostsList.as_view(), name='list'),
     url(r'^page=(?P<page>\d+)', PostsList.as_view()),
+    url(r'^(?P<slug>[\w-]+)/$', PostDetail.as_view(), name='detail'),
 
     url(r'^create', post_create, name='create'),
-    url(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
     url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name='edit'),
     url(r'^(?P<slug>[\w-]+)/add_comment/$', add_comment),
     url(r'^(?P<slug>[\w-]+)/delete/$', post_delete, name='delete'),
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -2,11 +2,49 @@ from django.contrib import messages
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from blog.comments.forms import CommentForm
 from .forms import PostForm
 from .models import Post
+
+
+class PostsList(ListView):
+    context_object_name = 'object_list'
+    model = Post
+    paginate_by = 10
+    template_name = 'posts/post_list.html'
+
+    def get_queryset(self):
+        if self.request.user.is_staff or self.request.user.is_superuser:
+            qs = Post.objects.all()
+        else:
+            qs = Post.objects.published()
+
+        query = self.request.GET.get('q')
+        if query:
+            qs = qs.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query)
+            ).distinct()
+        return qs
+
+
+class PostDetail(DetailView):
+    model = Post
+    context_object_name = 'instance'
+    template_name = 'posts/post_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data(**kwargs)
+
+        if not self.request.user.is_superuser:
+            self.object.add_view()
+            comments = self.object.comments.approved()
+        else:
+            comments = self.object.comments.all()
+        context['comments'] = comments
+        return context
 
 
 def post_create(request):
@@ -23,46 +61,6 @@ def post_create(request):
         'form': form,
     }
     return render(request, 'posts/post_form.html', context)
-
-
-def post_detail(request, slug=None):
-    instance = get_object_or_404(Post, slug=slug)
-    if not request.user.is_superuser:
-        instance.add_view()
-        comments = instance.comments.approved()
-    else:
-        comments = instance.comments.all()
-    context = {
-        'instance': instance,
-        'form': CommentForm,
-        'comments': comments
-    }
-    return render(request, 'posts/post_detail.html', context)
-
-
-class PostsList(ListView):
-    context_object_name = 'object_list'
-    model = Post
-    paginate_by = 10
-    template_name = 'posts/post_list.html'
-
-    context = {
-        'title': 'List'
-    }
-
-    def get_queryset(self):
-        if self.request.user.is_staff or self.request.user.is_superuser:
-            qs = Post.objects.all()
-        else:
-            qs = Post.objects.published()
-
-        query = self.request.GET.get('q')
-        if query:
-            qs = qs.filter(
-                Q(title__icontains=query) |
-                Q(content__icontains=query)
-            ).distinct()
-        return qs
 
 
 def post_update(request, slug):
