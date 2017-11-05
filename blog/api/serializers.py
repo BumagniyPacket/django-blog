de01ee3401f3226c51f8e3c0e333d8180f84ba8e
BumagniyPacket <bumagniypacket@yandex.ru>
2017-11-05 17:48:22 +0300

    upd api

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -6,4 +6,4 @@ from blog.comments.models import Comment
 class CommentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ('post', 'author', 'timestamp', 'text', 'approved')
+        fields = ('pk', 'post', 'author', 'timestamp', 'text', 'approved')
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,24 +1,14 @@
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
 
 from blog.api.serializers import CommentsSerializer
 from blog.comments.models import Comment
 
 
-class APICommentsList(APIView):
-    """API для работы со списком пользователей."""
+class APICommentsList(ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentsSerializer
 
-    def get(self, request):
-        """Доступ к списку пользоватей."""
-        customers = Comment.objects.all()
-        serializer = CommentsSerializer(customers, many=True)
-        return Response(serializer.data)
 
-    def post(self, request):
-        """Создание пользователя."""
-        serializer = CommentsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class APICommentDetail(RetrieveDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentsSerializer
