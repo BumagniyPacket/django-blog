993f9aa352e9ca9991c939286a583921bc5d9d7c
Roma <BumagniyPacket@yandex.ru>
2018-06-12 22:44:49 +0300

    articles list

--- apps/articles/urls.py
+++ apps/articles/urls.py
@@ -1,12 +1,7 @@
 from django.conf.urls import url
 
-from .views import ArticleCreateView, ArticleDeleteView, ArticleDetailView, ArticleUpdateView, ArticlesListView
+from .views import ArticlesListAPIView
 
 urlpatterns = [
-    url(r'^$', ArticlesListView.as_view(), name='list'),
-    url(r'^page=(?P<page>\d+)', ArticlesListView.as_view()),
-    url(r'^create', ArticleCreateView.as_view(), name='create'),
-    url(r'^(?P<slug>[\w-]+)$', ArticleDetailView.as_view(), name='detail'),
-    url(r'^(?P<slug>[\w-]+)/edit$', ArticleUpdateView.as_view(), name='edit'),
-    url(r'^(?P<slug>[\w-]+)/delete$', ArticleDeleteView.as_view(), name='delete'),
+    url(r'^$', ArticlesListAPIView.as_view(), name='list'),
 ]
