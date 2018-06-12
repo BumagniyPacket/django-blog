072768213edf430329fa198baaaa8445d6a57ee9
Roma <BumagniyPacket@yandex.ru>
2018-06-12 23:38:36 +0300

    ArticleSerializer -> ArticleDetail/ListSerializer

--- apps/articles/serializers.py
+++ apps/articles/serializers.py
@@ -3,7 +3,16 @@ from rest_framework import serializers
 from .models import Article
 
 
-class ArticleSerializer(serializers.ModelSerializer):
+class ArticleListSerializer(serializers.ModelSerializer):
+    url = serializers.ReadOnlyField(source='get_absolute_url',)
+    image = serializers.ReadOnlyField(source='image.url')
+
+    class Meta:
+        model = Article
+        fields = ('pk', 'title', 'url', 'image', 'description', 'timestamp')
+
+
+class ArticleDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
--- apps/articles/views.py
+++ apps/articles/views.py
@@ -1,14 +1,14 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from .models import Article
-from .serializers import ArticleSerializer
+from .serializers import ArticleDetailSerializer, ArticleListSerializer
 
 
 class ArticlesListAPIView(ListAPIView):
     queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
+    serializer_class = ArticleListSerializer
 
 
 class ArticleRetrieveView(RetrieveAPIView):
     queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
+    serializer_class = ArticleDetailSerializer
