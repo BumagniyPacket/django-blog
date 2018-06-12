e27722cac513ae51f93fddeb4d6ff56cbc817528
Roma <BumagniyPacket@yandex.ru>
2018-06-12 13:13:38 +0300

    upd settings

---
+++ core/settings/no_docker.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
