5541b13f8bb539fd15c430c86ecb6be7f55f8a22
Roma <BumagniyPacket@yandex.ru>
2018-06-17 22:43:39 +0300

    added article tags

--- apps/articles/admin.py
+++ apps/articles/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
-from .models import Article, Category
+from .models import Article, Category, Tag
 
 
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'draft')
-    list_filter = ('timestamp',)
+    list_filter = ('timestamp', 'tags')
     search_fields = ('title', 'description', 'content')
-    fields = ('title', 'category', 'image', 'description', 'content', 'draft')
+    fields = ('title', 'category', 'tags', 'image', 'description', 'content',
+              'draft')
 
     class Meta:
         model = Article
@@ -19,5 +20,12 @@ class CategoryAdmin(admin.ModelAdmin):
         fields = '__all__'
 
 
+class TagAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
 admin.site.register(Article, PostModelAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
--- apps/articles/models.py
+++ apps/articles/models.py
@@ -67,6 +67,11 @@ class Article(GenerateSlugMixin, models.Model):
         on_delete=models.CASCADE,
         blank=False
     )
+    tags = models.ManyToManyField(
+        'articles.Tag',
+        related_name='articles',
+        blank=False, null=False
+    )
 
     def __str__(self):
         return self.title
@@ -91,6 +96,17 @@ class Category(models.Model):
 
     name = models.CharField(verbose_name='Имя', max_length=35)
     description = models.TextField(verbose_name='Описание', max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    class Meta:
+        verbose_name = 'Блог - тег'
+        verbose_name_plural = 'Блог - теги'
+
+    name = models.CharField(verbose_name='Имя', max_length=35)
 
     def __str__(self):
         return self.name
