dc93c1885e9c893ea92c13293272e435b3f87f6c
Roma <BumagniyPacket@yandex.ru>
2018-06-17 21:28:13 +0300

    random background

--- apps/frontend/urls.py
+++ apps/frontend/urls.py
@@ -1,11 +1,17 @@
 from django.conf.urls import url
 
-from .views import BlogMainFrontendView, BlogDetailFrontendView
+from .views import (
+    BlogMainFrontendView, BlogDetailFrontendView, RandomBackgroundFrontendView,
+)
 
 urlpatterns = [
     url(r'^$', BlogMainFrontendView.as_view(), name='list'),
     url(
-        r'^(?P<pk>\w+)/$', BlogDetailFrontendView.as_view(),
+        r'^article/(?P<pk>\w+)/$', BlogDetailFrontendView.as_view(),
         name='article-detail'
-    )
+    ),
+    url(
+        r'^bg/$', RandomBackgroundFrontendView.as_view(),
+        name='blog-background'
+    ),
 ]
--- apps/frontend/views.py
+++ apps/frontend/views.py
@@ -1,4 +1,10 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from random import choice
+
 from django.views.generic import DetailView, ListView
+from django.views import View
+from filer.models import Folder
 
 from apps.articles.models import Article
 
@@ -11,3 +17,11 @@ class BlogMainFrontendView(ListView):
 class BlogDetailFrontendView(DetailView):
     model = Article
     template_name = 'frontend/detail_article.html'
+
+
+class RandomBackgroundFrontendView(View):
+    def get(self, *args, **kwargs):
+        folder = get_object_or_404(Folder, name='backgrounds')
+        images = folder.files
+        image = choice(images)
+        return HttpResponseRedirect(image.url)
