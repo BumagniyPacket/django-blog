aa754fc64e1bdac1210796e11c3366ca2c33a230
Roma <BumagniyPacket@yandex.ru>
2018-06-17 17:50:13 +0300

    search by description

--- apps/articles/admin.py
+++ apps/articles/admin.py
@@ -6,7 +6,7 @@ from .models import Article
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ('title', 'timestamp', 'updated', 'draft')
     list_filter = ('timestamp',)
-    search_fields = ('title', 'content')
+    search_fields = ('title', 'description', 'content')
     fields = ('title', 'image', 'description', 'content', 'draft')
 
     class Meta:
